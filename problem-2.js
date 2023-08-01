fc34c57620dfe444ab0d2d56bc10633d5d519be8
Saifuzzaman <pmsaifuzzaman@gmail.com>
2023-08-01 17:08:48 +0600

    problem-2

---
+++ problem-2.js
@@ -0,0 +1,15 @@
+// Display number between 58 to 98
+
+
+// Using for loop
+var i = 58 ;
+for(var i =58; i <= 98 ; i++){
+    console.log(i);
+}
+
+// Using While loop
+var i = 58 ;
+while(i <= 98){
+    console.log(i);
+    i++ ;
+}
