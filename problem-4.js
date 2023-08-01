2d68c408caad6039fb6a7a0208f440857e5c4dfc
Saifuzzaman <pmsaifuzzaman@gmail.com>
2023-08-01 17:20:30 +0600

    problem-4

---
+++ problem-4.js
@@ -0,0 +1,16 @@
+// Show all odd number between 581 to 623
+
+// Using for loop
+
+var i = 581 ;
+for(var i =581 ; i<=623 ; i+=2){
+    console.log(i);
+}
+
+// Using while loop
+
+var i = 581 ;
+while(i <= 623){
+    console.log(i);
+    i+=2 ;
+}
