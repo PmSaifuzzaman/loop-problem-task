d522da6cea6bf1b4911a15cfa172a27736403969
Saifuzzaman <pmsaifuzzaman@gmail.com>
2023-08-01 17:14:26 +0600

    problem-3

---
+++ problem-3.js
@@ -0,0 +1,16 @@
+// Show all even number between 412 to 456
+
+//Using for loop
+
+var i = 412;
+for(var i = 412; i <= 456 ; i+=2){
+    console.log(i);
+}
+
+// Using while loop
+
+var i = 412;
+while(i<=456){
+    console.log(i);
+    i+=2 ;
+}
