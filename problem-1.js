64dc6ddf1a256fb2bc74b01d698e407edf182bc4
Saifuzzaman <pmsaifuzzaman@gmail.com>
2023-08-01 17:01:51 +0600

    first commit

---
+++ problem-1.js
@@ -0,0 +1,20 @@
+// Display "Javascript no.1 programming laguage"  39 times
+
+// Using for loop
+
+var i = 1;
+for (var i = 1 ; i <=39 ; i++){
+    console.log("Javascript no.1 programming laguage");
+    console.log(i);
+}
+
+
+
+// using while loop
+
+var pathik = "Javascript no.1 programming laguage";
+var i = 1;
+while(i <= 39){
+    console.log(i  + " "  + pathik);
+    i++ ;
+}
