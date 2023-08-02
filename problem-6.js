5fed54599b95b694bcb67d725a0842eae3c51f7d
Saifuzzaman <pmsaifuzzaman@gmail.com>
2023-08-02 13:58:52 +0600

    Problem-6

---
+++ problem-6.js
@@ -0,0 +1,8 @@
+// Declear an array for all topics you have learned and display them as output
+
+
+var iHaveLearned = ['Tailwind', 'Daisyui', 'HTML', 'CSS', 'Javascript', ]
+for(var i = 0; i < iHaveLearned.length ; i++){
+    var singleTopic = iHaveLearned[i];
+    console.log(singleTopic);
+}
