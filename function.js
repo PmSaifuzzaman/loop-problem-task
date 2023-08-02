7dab67c38ec2f84d0ba3f34b2bfe59d3cf4e0967
Saifuzzaman <pmsaifuzzaman@gmail.com>
2023-08-03 00:27:57 +0600

    function

---
+++ function.js
@@ -0,0 +1,14 @@
+
+// Basic Sum function
+function addNumbers(a,b){
+    return a+b ;
+}
+const sum = addNumbers(8,9);
+console.log(sum);
+
+// Basic Square output
+function square(num){
+    return num*num;
+}
+var squre2 = square(9);
+console.log(squre2);
--- problem-6.js
+++ problem-6.js
@@ -1,8 +1,19 @@
 // Declear an array for all topics you have learned and display them as output
 
+// For loop
 
 var iHaveLearned = ['Tailwind', 'Daisyui', 'HTML', 'CSS', 'Javascript', ]
 for(var i = 0; i < iHaveLearned.length ; i++){
     var singleTopic = iHaveLearned[i];
     console.log(singleTopic);
+}
+
+// While loop
+
+var iHaveLearned = ['Tailwind', 'Daisyui', 'HTML', 'CSS', 'Javascript', ]
+var i = 0;
+while(i < iHaveLearned.length){
+    var singleTopic = iHaveLearned[i];
+    console.log(singleTopic);
+    i++;
 }
